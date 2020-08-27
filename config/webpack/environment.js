92b92a136ca9376a2496ecbf8016619a22b52294
f.taka <98.tenpwayway@gmail.com>
2020-08-27 09:43:02 +0900

    エラー修正

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,16 +1,3 @@
-const webpack = require('webpack')
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-    new webpack.ProvidePlugin({
-        $: 'jquery/src/jquery',
-        jQuery: 'jquery/src/jquery'
-    })
-)
-environment.plugins.append(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-  })
-)
+const { environment } = require('@rails/webpacker')
+
+module.exports = environment
